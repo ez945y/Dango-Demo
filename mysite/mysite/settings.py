c9b13373ebbe48b5bd9c67c51bc569602483b714
mike <ez945y@gmail.com>
2022-02-08 20:35:29 +0800

    1.2.0

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 FIXTURE_DIRS = [
     os.path.join(BASE_DIR, 'fixtures')
